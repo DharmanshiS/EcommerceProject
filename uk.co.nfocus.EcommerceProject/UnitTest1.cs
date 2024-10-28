e44a9f2fa9522ffae1dbcf916c2aace78f5525b7
DharmanshiSangani <DharmanshiSangani@NF-NB02>
2024-10-28 16:42:37 +0000

    Add project files.

---
+++ uk.co.nfocus.EcommerceProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace uk.co.nfocus.EcommerceProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
